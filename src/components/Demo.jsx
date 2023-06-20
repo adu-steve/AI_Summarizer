c5729de9fbe78a0ec8eb6306fbf957c3fe685af1
Stephen Adu <97965000+adu-steve@users.noreply.github.com>
2023-06-20 23:16:12 +0000

    New Creation of the Hero section and the Demo section in place

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,5 +1,38 @@
+import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
+
 const Demo = () => {
-  return <div>Demo</div>;
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      {/*Search*/}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative-flex justify-center items-center"
+          onSubmit={() => {}}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-100 my-3 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value=""
+            onChange={() => {}}
+            required
+            className="url_input peer w-full"
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            ^
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Demo;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,27 @@ import { logo } from "../assets";
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      <nav className="flex justify-between items-center flex-col">
-        <img src={logo} alt="sumz_logo" className="w-28" />
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => {
+            window.open("https://github.com/adu-steve/AI_Summarizer");
+          }}
+          className="black_btn"
+        >
+          Github
+        </button>
       </nav>
+      <h1 className="head_text glassmorphism ">
+        {" "}
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OPENAI</span>
+      </h1>
+      <h2 className="desc">
+        Summarize your reading with Sumz, an open-source article summarizer that
+        transforms lengthy articles into clear and concise summaries.{" "}
+      </h2>
     </header>
   );
 };
