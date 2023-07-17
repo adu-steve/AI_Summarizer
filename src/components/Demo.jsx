48a476ced4cd28a9fb1ed18d431c894c9eb5465b
Stephen Adu <97965000+adu-steve@users.noreply.github.com>
2023-07-17 00:12:35 +0000

    Store to control the entire state complete using redux configStore plus fetching article article summarization's ai using rapidafi feature as well

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,11 +1,21 @@
 import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 import { MdArrowCircleUp } from "react-icons/md";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
   const [article, setArtcle] = useState({ url: "", summary: "" });
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
-  const handleSubmit = async (e) => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await getSummary({ articleUrl: article.url });
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      setArtcle(newArticle);
+      console.log(newArticle);
+    }
+  };
   return (
     <section className="mt-16 w-full max-w-xl">
       {/*Search*/}
